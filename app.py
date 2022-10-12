3a0d16432bdda5d7cf21ab650e4199430a4cd05f
cmarsh47 <chanpmarshall21@gmail.com>
2022-10-12 18:32:04 -0400

    Adding index.html

--- app.py
+++ app.py
@@ -6,19 +6,22 @@ import datetime
 app = Flask(__name__)
 global studentOrganisationDetails
 # Assign default 5 values to studentOrganisationDetails for Application  3.
+studentOrganisationDetails = dict()
 
 
 @app.get('/')
 def index():
     # Complete this function to get current date and time assign this value to currentDate, display this data on index.html
-
+    currentDate = datetime.datetime.now()
+    currentDate = currentDate.strftime('%Y-%m-%d %H:%M:%S')
+    
     return render_template('index.html', currentDate=currentDate)
 
 
 @app.get('/calculate')
 def displayNumberPage():
     # Complete this function to display form.html page
-    pass
+    return render_template('form.html')
 
 
 @app.route('/calculate', methods=['POST'])
@@ -30,6 +33,14 @@ def checkNumber():
     # Display "Provided input is not an integer!" if value is not a number on result.html page
     global number
     number = request.form['number']
+    try:
+        if int(number)%2==0:
+            msg='even'
+        elif int(number)%2!=0:
+            msg='odd'
+    except:
+        msg='not an integer!'
+    return render_template('result.html', num=number, name=msg)
 
     # Write your to code here to check whether number is even or odd and render result.html page
 
@@ -37,13 +48,22 @@ def checkNumber():
 @app.get('/addStudentOrganisation')
 def displayStudentForm():
     # Complete this function to display studentFrom.html page
-    pass
+     return render_template(
+        'studentForm.html',
+        organizations=[{'orgName': 'Flipcart'}, {'orgName': 'Amazon'}, {'orgName': 'CSSolver'}, {'orgName': 'Scaler'}, {'orgName': 'Tryjaar'}])
 
 
 @app.route('/addStudentOrganisation', methods=['POST'])
 def displayRegistrationPage():
     # Get student name and organisation from form.
     studentName = request.form['name']
+    orgNAme = request.form['organization']
+    
+    studentOrganisationDetails[studentName] = orgNAme
+    
+    return render_template(
+        'studentDetails.html',
+        studentOrganisationDetails=studentOrganisationDetails)
 
     # Append this value to studentOrganisationDetails
 
